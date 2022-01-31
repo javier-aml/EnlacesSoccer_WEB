15b2234c30d701c841fb81c4c5fb9f1799618674
Javier <javier_aml@outlook.com>
2022-01-31 09:39:56 -0600

    Test

--- src/router/index.js
+++ src/router/index.js
@@ -84,11 +84,7 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
-  navigationFallback: {
-    rewrite: "/index.html",
-    exclude: ["/images/*.{png,jpg,gif}", "/css/*"]
-  }
+  routes
 })
 
 export default router
