73122f1a9f54b5d5a7db270f067b4b98e746178f
Javier <javier_aml@outlook.com>
2022-03-16 08:22:38 -0600

    Se corrige version movil del grid

--- src/plugins/vuetify/index.js
+++ src/plugins/vuetify/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
 import preset from './default-preset/preset'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify)
 
--- src/views/pages/Grid2.vue
+++ src/views/pages/Grid2.vue
@@ -1,14 +1,67 @@
 <template>
     <v-data-table 
-        :headers="headers" 
-        :items="items" 
+        :items="isFiltered ? itemsFilter : items" 
         :search="search" 
         class="elevation-1" 
-        fixed-header 
         height="350px"
+        :mobile-breakpoint='NaN'
     >
+        <template #header :headers="headers">
+            <thead v-if="!isMobile()">
+                <tr>
+                    <th
+                        v-for="header in headers" 
+                        :key="header.value"
+                    >
+                        <span>
+                            <v-menu offset-y :close-on-content-click="false">
+                                <template v-slot:activator="{on, attrs}">
+                                    {{header.text}}
+                                    <v-btn 
+                                        icon v-bind="attrs" 
+                                        v-on="on"
+                                        v-if="header.value !== 'eliminar'"
+                                    >
+                                        <v-icon small :disabled="header.filter ? false : true">mdi-filter</v-icon>
+                                    </v-btn>
+                                </template>
+                                <div style="background-color: white; width: 280px">
+                                    <v-text-field
+                                        class="pa-4"
+                                        :label="`Filtrar por ${header.text} :`"
+                                        :autofocus="true"
+                                        :value="header.filter"
+                                        @change="onFilter($event, header.value)"
+                                    ></v-text-field>
+                                    <v-btn
+                                        small
+                                        text
+                                        color="primary"
+                                        class="ml-2 mb-2"
+                                        @click="clearFilter($event, header.value)"
+                                    >Limpiar</v-btn>
+                                </div>
+                            </v-menu>
+                        </span>
+                    </th>
+                </tr>
+            </thead>
+            <thead v-else>
+                <tr>
+                    <th>
+                        Filtros
+                        <v-btn 
+                            icon v-bind="attrs" 
+                            v-on="on"
+                        >
+                            <v-icon small>mdi-filter</v-icon>
+                        </v-btn>
+                    </th>
+                </tr>
+            </thead>
+        </template>
         <template #item="{item}">
-            <tr>
+            <tr v-if="!isMobile()">
                 <td 
                     v-for="cell in Object.keys(item)" 
                     :key="cell.id" :cell-name="cell" 
@@ -34,6 +87,7 @@
                         @change="onChangeCell($event, item.id, cell)"
                         v-else-if="edited(item,cell) && dataType(cell) === 'combo'"
                         :items="combos[cell]"
+                        :value="1"
                         item-text="Nom"
                         item-value="Id"
                     ></v-select>
@@ -45,6 +99,53 @@
                     </span>
                 </td>
             </tr>
+            <tr v-else>
+                <td 
+                    class="v-data-table__mobile-row"
+                    v-for="cell in Object.keys(item)" 
+                    :key="cell.id" :cell-name="cell" 
+                    :cell-id="item.id"
+                >
+                    <div class="v-data-table__mobile-row__header">
+                        {{headerText(cell)}}
+                    </div>
+                    <div class="v-data-table__mobile-row__cell" style="float:rigth">
+                        <v-checkbox
+                            @change="onChangeCell($event, item.id, cell)"
+                            v-if="dataType(cell) === 'delete'"
+                        ></v-checkbox>
+                        <v-checkbox
+                            @change="onChangeCell($event, item.id, cell)"
+                            v-else-if="dataType(cell) === 'check'"
+                        ></v-checkbox>
+                        <v-text-field
+                            @change="onChangeCell($event, item.id, cell)"
+                            :value="item[cell]"
+                            v-else-if="edited(item,cell) && dataType(cell) === 'text'"
+                            dense single-line
+                            style="max-width: 150px"
+                        >
+                        </v-text-field>
+                        <v-select
+                            @change="onChangeCell($event, item.id, cell)"
+                            v-else-if="edited(item,cell) && dataType(cell) === 'combo'"
+                            :items="combos[cell]"
+                            :value="1"
+                            item-text="Nom"
+                            item-value="Id"
+                            style="max-width: 150px"
+                        ></v-select>
+                        <span
+                            v-else
+                            @click="onEditRow($event, item.id, cell)"
+                            style="max-width: 150px"
+                        >
+                            {{item[cell]}}
+                        </span>
+                    </div>
+                </td>
+                <hr>
+            </tr>
         </template>
     </v-data-table>
 </template>
@@ -52,17 +153,18 @@
     export default{
         data: () => ({
             headers: [
-                {text: 'Nombre', value: 'nombre', sortable: true, width: '150px', type: 'text'},
-                {text: 'Apellido', value: 'apellido', sortable: true, width: '150px', type: 'text'},
-                {text: 'Telefono', value: 'telefono', sortable: false, width: '150px', type: 'text'},
-                {text: 'Pais', value: 'pais', sortable: true, width: '150px', type: 'combo'},
-                {text: 'Valido', value: 'valido', sortable: false, width: '150px', type: 'check'},
-                {text: 'Eliminar', value: 'eliminar', sortable: false, width: '150px', type: 'delete'}
+                {text: 'Nombre', value: 'nombre', sortable: true, width: '150px', type: 'text', filter: ''},
+                {text: 'Apellido', value: 'apellido', sortable: true, width: '150px', type: 'text', filter: ''},
+                {text: 'Telefono', value: 'telefono', sortable: false, width: '150px', type: 'text', filter: ''},
+                {text: 'Pais', value: 'pais', sortable: true, width: '150px', type: 'combo', filter: ''},
+                {text: 'Valido', value: 'valido', sortable: false, width: '150px', type: 'check', filter: ''},
+                {text: 'Eliminar', value: 'eliminar', sortable: false, width: '150px', type: 'delete', filter: ''}
             ],
             items: [
                 {id: 1, nombre: 'Javier', apellido: 'Arredondo', telefono: 4612347082, pais: 'Mexico', valido: false, eliminar: false, edit: []},
                 {id: 2, nombre: 'Diego', apellido: 'Arredondo', telefono: 6121184026, pais: 'EUA', valido: false, eliminar: false, edit: []}
             ],
+            itemsFilter: [],
             combos: {
                 pais: [
                     {Id: 1, Nom: 'Mexico'},
@@ -94,8 +196,31 @@
                 const index = this.headers.findIndex(item => item.value === cell);
                 return this.headers[index] ? this.headers[index].type : '';
             },
+            headerText(cell){
+                const index = this.headers.findIndex(item => item.value === cell);
+                return this.headers[index] ? this.headers[index].text : '';
+            },
             edited(item, cell){
                 return item.edit.includes(cell);
+            },
+            onFilter(val, col){
+                const index = this.headers.findIndex(item => item.value === col);
+                this.headers[index].filter = val;
+                this.itemsFilter = [];
+                this.itemsFilter = this.items.filter(item => item[col].includes(val));
+            },
+            clearFilter(val, col){
+                const index = this.headers.findIndex(item => item.value === col);
+                this.headers[index].filter = '';
+                this.itemsFilter = [];
+            },
+            isMobile(){
+                return window.innerWidth < 600 ? true : false;
+            }
+        },
+        computed: {
+            isFiltered(){
+                return this.headers.some(item => item.filter);
             }
         }
     }
