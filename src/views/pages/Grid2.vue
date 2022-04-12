a85e0b0e25cb763bca57543e357cdd65631ac43c
Javier <javier_aml@outlook.com>
2022-04-12 09:47:09 -0500

    Se corrigen avanza en filtros de la version de escritorio, se reemplaza el componente de fecha, se corrige filtro de fecha, se corrigen varios errores en los filtros

--- src/views/pages/Grid2.vue
+++ src/views/pages/Grid2.vue
@@ -1,6 +1,6 @@
 <template>
     <v-data-table 
-        :items="isFiltered ? itemsFilter : items" 
+        :items="isFiltered ? itemsFilter : items"  
         :search="search" 
         class="elevation-1" 
         height="450px"
@@ -32,35 +32,87 @@
                                         :label="`${header.text} :`"
                                         :autofocus="true"
                                         :value="header.filter"
-                                        @change="onFilter($event, header.value)"
+                                        @input="onFilter($event, header.value)"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        v-if="header.type === 'email'"
+                                        class="pa-4"
+                                        :label="`${header.text} :`"
+                                        :autofocus="true"
+                                        :value="header.filter"
+                                        @input="onFilter($event, header.value)"
                                     ></v-text-field>
                                     <span
                                         v-if="header.type === 'date'"
                                     >
-                                        <v-text-field
-                                            class="pa-4"
-                                            :label="`De :`"
-                                            :autofocus="true"
-                                            :value="header.filter"
-                                            type="date"
-                                            @change="onFilter($event, header.value)"
-                                        ></v-text-field>
-                                        <v-text-field
-                                            class="pa-4"
-                                            :label="`A :`"
-                                            :autofocus="true"
-                                            :value="header.filter"
-                                            type="date"
-                                            @change="onFilter($event, header.value)"
-                                        ></v-text-field>
+                                        <v-menu
+                                            :close-on-content-click="false"
+                                            transition="scale-transition"
+                                            offset-y
+                                        >
+                                            <template v-slot:activator="{on}">
+                                                <v-text-field
+                                                    :value="header.filter.from"
+                                                    label="De :"
+                                                    readonly
+                                                    v-on="on"
+                                                    class="pa-4"
+                                                >
+                                                </v-text-field>
+                                            </template>
+                                            <v-date-picker
+                                                locale="es-mx"
+                                                v-model="header.filter.from"
+                                                @change="onFilter($event, header.value)"
+                                            >
+                                            </v-date-picker>
+                                        </v-menu>
+                                        <v-menu
+                                            :close-on-content-click="false"
+                                            transition="scale-transition"
+                                            offset-y
+                                        >
+                                            <template v-slot:activator="{on}">
+                                                <v-text-field
+                                                    :value="header.filter.to"
+                                                    label="A :"
+                                                    readonly
+                                                    v-on="on"
+                                                    class="pa-4"
+                                                    style="margin-top: -30px;"
+                                                >
+                                                </v-text-field>
+                                            </template>
+                                            <v-date-picker
+                                                locale="es-mx"
+                                                v-model="header.filter.to"
+                                                @change="onFilter($event, header.value)"
+                                            >
+                                            </v-date-picker>
+                                        </v-menu>
                                     </span>
+                                    <v-text-field-simplemask
+                                        v-if="header.type === 'telephone'"
+                                        class="pa-4"
+                                        :label="`${header.text} :`"
+                                        :autofocus="true"
+                                        v-model="header.filter"
+                                        @input="onFilter($event, header.value)"
+                                        :options="{
+                                            inputMask: '(###) #######',
+                                            outputMask: '##########',
+                                            empty: null,
+                                            applyAfter: false,
+                                            alphanumeric: false,
+                                            lowerCase: false,
+                                        }"
+                                    ></v-text-field-simplemask>
                                     <v-select
                                         v-else-if="header.type === 'combo'"
                                         :items="combos[header.value]"
                                         :value="header.filter"
                                         item-text="Nom"
                                         item-value="Id"
-                                        style="max-width: 150px"
                                         class="pa-4"
                                         :label="`${header.text} :`"
                                         :autofocus="true"
@@ -112,10 +164,15 @@
                         :key="header.id"
                         style="background: #dcdcdc"
                     >
-                        <div class="v-data-table__mobile-row__header">
-                            {{header.text}}
+                        <div 
+                            class="v-data-table__mobile-row__header"
+                        >
+                            {{header.type !== 'delete' ? header.text: ''}}
                         </div>
-                        <div class="v-data-table__mobile-row__cell" style="float:rigth">
+                        <div 
+                            class="v-data-table__mobile-row__cell" 
+                            style="float:rigth"
+                        >
                             <v-checkbox
                                 v-if="header.type === 'check'"
                                 style="max-width: 150px"
@@ -126,6 +183,39 @@
                                 style="max-width: 150px"
                             >
                             </v-text-field>
+                            <v-text-field-simplemask
+                                v-if="header.type === 'telephone'"
+                                v-model="header.filter"
+                                :options="{
+                                    inputMask: '(###) #######',
+                                    outputMask: '##########',
+                                    empty: null,
+                                    applyAfter: false,
+                                    alphanumeric: false,
+                                    lowerCase: false,
+                                }"
+                                style="max-width: 150px"
+                            ></v-text-field-simplemask>
+                            <v-text-field
+                                v-else-if="header.type === 'email'"
+                                dense single-line
+                                style="max-width: 150px"
+                            >
+                            </v-text-field>
+                            <span
+                                v-else-if="header.type === 'date'"
+                            >
+                                <v-text-field
+                                    :label="`De :`"
+                                    type="date"
+                                    style="max-width: 150px"
+                                ></v-text-field>
+                                <v-text-field
+                                    :label="`A :`"
+                                    type="date"
+                                    style="max-width: 150px"
+                                ></v-text-field>
+                            </span>
                             <v-select
                                 v-else-if="header.type === 'combo'"
                                 :items="combos[header.value]"
@@ -134,6 +224,24 @@
                                 item-value="Id"
                                 style="max-width: 150px"
                             ></v-select>
+                            <span
+                                v-else-if="header.type === 'delete'"
+                            >
+                                <v-btn
+                                    depressed
+                                    color="primary"
+                                    style="margin-bottom: 10px;"
+                                >
+                                    Limpiar
+                                </v-btn>                            
+                                <v-btn
+                                    depressed
+                                    color="primary"
+                                    style="margin-bottom: 10px; margin-left: 10px;"
+                                >
+                                    Filtrar
+                                </v-btn>
+                            </span>
                         </div>
                     </td>                
                 </tr>
@@ -179,11 +287,32 @@
                     <v-text-field
                         @change="onChangeCell($event, item.id, cell)"
                         :value="item[cell]"
-                        type="date"
-                        v-else-if="edited(item,cell) && dataType(cell) === 'date'"
+                        v-else-if="edited(item,cell) && dataType(cell) === 'email'"
                         dense single-line
+                        style="max-width: 150px"
                     >
                     </v-text-field>
+                    <v-menu
+                        v-else-if="edited(item,cell) && dataType(cell) === 'date'"
+                        :close-on-content-click="false"
+                        transition="scale-transition"
+                        offset-y
+                    >
+                        <template v-slot:activator="{on}">
+                            <v-text-field
+                                :value="item[cell]"
+                                readonly
+                                v-on="on"
+                            >
+                            </v-text-field>
+                        </template>
+                        <v-date-picker
+                            locale="es-mx"
+                            :value="item[cell]"
+                            @change="onChangeCell($event, item.id, cell)"
+                        >
+                        </v-date-picker>
+                    </v-menu>
                     <v-select
                         @change="onChangeCell($event, item.id, cell)"
                         v-else-if="edited(item,cell) && dataType(cell) === 'combo'"
@@ -227,6 +356,30 @@
                             style="max-width: 150px"
                         >
                         </v-text-field>
+                        <v-text-field-simplemask
+                            :value="item[cell]"
+                            :ref="cell"
+                            v-else-if="edited(item,cell) && dataType(cell) === 'telephone'"
+                            dense single-line
+                            @input="onChangeCell($event, item.id, cell)"
+                            :options="{
+                                inputMask: '(###) #######',
+                                outputMask: '##########',
+                                empty: null,
+                                applyAfter: false,
+                                alphanumeric: false,
+                                lowerCase: false,
+                            }"
+                        >
+                        </v-text-field-simplemask>
+                        <v-text-field
+                            @change="onChangeCell($event, item.id, cell)"
+                            :value="item[cell]"
+                            v-else-if="edited(item,cell) && dataType(cell) === 'email'"
+                            dense single-line
+                            style="max-width: 150px"
+                        >
+                        </v-text-field>
                         <v-text-field
                             @change="onChangeCell($event, item.id, cell)"
                             :value="item[cell]"
@@ -265,14 +418,15 @@
             headers: [
                 {text: 'Nombre', value: 'nombre', sortable: true, width: '150px', type: 'text', filter: null},
                 {text: 'Telefono', value: 'telefono', sortable: false, width: '150px', type: 'telephone', filter: null},
-                {text: 'Fecha', value: 'fecha', sortable: false, width: '150px', type: 'date', filter: null},
+                {text: 'E-Mail', value: 'correo', sortable: false, width: '150px', type: 'email', filter: null},
+                {text: 'Fecha', value: 'fecha', sortable: false, width: '150px', type: 'date', filter: {from: null, to: null}},
                 {text: 'Pais', value: 'pais', sortable: true, width: '150px', type: 'combo', filter: null},
                 {text: 'Valido', value: 'valido', sortable: false, width: '150px', type: 'check', filter: null},
                 {text: 'Eliminar', value: 'eliminar', sortable: false, width: '150px', type: 'delete', filter: null}
             ],
             items: [
-                {id: 1, nombre: 'Javier', telefono: 4612347082, fecha: '2022-03-21', pais: 1, valido: false, eliminar: false, edit: []},
-                {id: 2, nombre: 'Diego', telefono: 6121184026, fecha: '2022-03-21', pais: 2, valido: false, eliminar: false, edit: []}
+                {id: 1, nombre: 'Javier', telefono: 4612347082, correo: 'javier_aml@outlook.com', fecha: '2022-03-21', pais: 1, valido: false, eliminar: false, edit: []},
+                {id: 2, nombre: 'Diego', telefono: 6121184026, correo: 'diego_aml@outlook.com', fecha: '2022-03-21', pais: 2, valido: false, eliminar: false, edit: []}
             ],
             itemsFilter: [],
             combos: {
@@ -282,10 +436,14 @@
                 ]},
             search: '',
             itemsEdit: [],
-            mobileFilter: false
+            mobileFilter: false,
+            rules: {
+                required: value => !!value || 'Required.'
+            }
         }),
         methods: {
             onChangeCell(val, rowId, cell){
+                console.log(val);
                 if(!val) return;
                 const index = this.itemsEdit.findIndex(item => item.id === rowId);
                 if(index !== -1) this.itemsEdit[index][cell] = val;
@@ -294,7 +452,6 @@
                     row[cell] = val;
                     this.itemsEdit.push(row);
                 }
-                console.log(val);
             },
             onEditRow(event, rowId, cell){
                 const index = this.items.findIndex(item => item.id === rowId);
@@ -316,18 +473,35 @@
                 return item.edit.includes(cell);
             },
             onFilter(val, col){
+                if(!val) return;
                 const index = this.headers.findIndex(item => item.value === col);
-                this.headers[index].filter = val;
+                const header = this.headers[index];
                 const dataType = this.headers[index].type;
                 if(dataType === 'combo'){
+                    this.headers[index].filter = val;
                     this.itemsFilter = this.items.filter(item => item[col] === val);
+                }else if (dataType === 'date'){
+                    const dateFilter = (filter, val) => {
+                        if(header.filter.hasOwnProperty('from') && header.filter.hasOwnProperty('to')){
+                            if(!header.filter.from || !header.filter.to) return true;
+                            else if(new Date(header.filter.from) <= new Date(filter) && new Date(filter) <= new Date(header.filter.to)) return true;
+                            else return false;
+                        } 
+                        else return false;
+                    }
+                    this.itemsFilter = this.items.filter(item => dateFilter(item[col], val));
                 }else{
-                    this.itemsFilter = this.items.filter(item => item[col].includes(val));
+                    this.headers[index].filter = val;
+                    this.itemsFilter = this.items.filter(item => (item[col] + '').toLowerCase().includes((val + '').toLowerCase()));
                 }
             },
             clearFilter(val, col){
                 const index = this.headers.findIndex(item => item.value === col);
-                this.headers[index].filter = '';
+                if(this.headers[index].type === 'date'){
+                    this.headers[index].filter.from = '';
+                    this.headers[index].filter.to = '';
+                }
+                else this.headers[index].filter = '';
                 this.itemsFilter = [];
             },
             isMobile(){
@@ -349,7 +523,16 @@
         },
         computed: {
             isFiltered(){
-                return this.headers.some(item => item.filter);
+                const isFiltered = filter => {
+                    if(filter){
+                        if(typeof filter === 'object')
+                            if(filter.hasOwnProperty('from')) return filter.from ? true : false;
+                            else if(filter.hasOwnProperty('to')) return filter.to ? true : false;
+                            else return true;
+                        else return true;
+                    }else return false;
+                }
+                return this.headers.some(item => isFiltered(item.filter));
             }
         }
     }
