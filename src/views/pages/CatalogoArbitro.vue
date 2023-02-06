4de937bfe85ef0479289292542a3cf96212d845c
Arturo <arturo.rodriguez.abud@gmail.com>
2023-02-06 09:15:13 -0600

    actualizo cat de arbitros, falta habilitar guardar

--- src/views/pages/CatalogoArbitro.vue
+++ src/views/pages/CatalogoArbitro.vue
@@ -13,50 +13,18 @@
           <v-row>
             
             <v-col cols="12">
-            <Grid
-              :header-prop="gridHeader"
-              :data-prop="gridData"
-              :combo-prop="gridCombo"
+              <Grid
+                :header-prop="gridHeader"
+                :data-sel-prop="gridDataSel"
+                :data-ui-prop="gridDataUi"
+                :combo-prop="gridCombo"
+                :key-prop="gridKey"
               ></Grid>
-            </v-col>
-
-            <v-col cols="12">
-              <v-btn
-                :color="color"
-                class="me-3 mt-4"
-                :disabled="habilitaGuardar"
-                @click.prevent="guardaArbitro"
-              >
-                Guardar
-              </v-btn>
             </v-col>
           </v-row>
         </v-form>
       </v-card-text>
     </v-card>
-
-    <v-card
-      style="margin-left:auto; margin-right:auto; margin-top: 10%;"
-      width="500px"
-      height="200px"
-
-      v-show="esGuardado"
-    >
-      <v-card-title
-        style="padding-top:50px"
-        class="justify-center"
-      >
-        El arbitro fue registrado correctamente.
-      </v-card-title>
-      <v-btn
-        :color="color"
-        style="margin-left:190px; margin-right:auto; margin-top: 10px;"
-        @click.prevent="confirmaGuardar"
-      >
-        Aceptar
-      </v-btn>
-    </v-card>
-
   </span>
 </template>
 
@@ -74,15 +42,17 @@
           valMsg: 'Este campo es requerido',
           color: '#03A9F4',
           gridHeader: [
-            {text: 'Liga', value: 'IdLiga', sortable: true, width: '250px', type: 'combo'},
-            {text: 'IdArbitro', value: 'IdArbitro', sortable: false, width: '50px', type: 'number'},
-            {text: 'Nombre', value: 'Nombre', sortable: false, width: '500px', type: 'text'},
-            {text: 'Telefono', value: 'Telefono', sortable: false, width: '50px', type: 'text'},
-            {text: 'Juegos', value: 'JuegosArbitrados', sortable: false, width: '50px', type: 'number'},
-            {text: 'Activo', value: 'Activo', sortable: false, width: '50px', type: 'check'}
+            {text: 'Liga', value: 'IdLiga', sortable: true, width: '150px', type: 'combo', editable: true, ui: true, visible: true},
+            {text: 'IdArbitro', value: 'IdArbitro', sortable: true, width: '50px', type: 'number', editable: true, ui: true, visible: true},
+            {text: 'Nombre', value: 'Nombre', sortable: false, width: '250px', type: 'text', editable: true, ui: true, visible: true},
+            {text: 'Telefono', value: 'Telefono', sortable: false, width: '50px', type: 'text', editable: true, ui: true, visible: true},
+            {text: 'Juegos', value: 'JuegosArbitrados', sortable: true, width: '50px', type: 'number', editable: true, ui: true, visible: true},
+            {text: 'Activo', value: 'Activo', sortable: false, width: '150px', type: 'check', editable: true, ui: true, visible: true},
           ],
           gridCombo: [{name: 'IdLiga', data: 'getLigas', default: 1}],
-          gridData: 'Arbitro'
+          gridDataSel: 'BuscarArbitros',
+          gridDataUi: 'GuardarArbitro',
+          gridKey: ['IdLiga','IdArbitro']
       }
     },
     computed: {
@@ -94,23 +64,6 @@
         }
         return dataArr;
       },
-      habilitaGuardar() {
-        if(this.Nombre && this.Idliga) return false;
-        else return true;
-      }
-    },
-    methods: {
-      async guardarArbitro() {
-        const data = await this.$store.dispatch('postGuardarArbitro', {
-          IdLiga: this.IdLiga,
-          Nombre: this.Nombre
-        });
-        this.esGuardado = data === 1 ? true : false;
-      },
-      confirmaGuardar(){
-        this.$refs.forma.reset();
-        this.esGuardado = false;
-      }
     },
     async mounted() {
       await this.$store.dispatch('getLigas');
